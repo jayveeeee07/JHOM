4fdd67bd0eb7aebe978794982cad91b09faf5fc1
Jayvee Caringal <144330185+jayveeeee07@users.noreply.github.com>
2025-08-08 21:33:22 +0800

    Update server.php

--- server.php
+++ server.php
@@ -1,42 +1,28 @@
 <?php
 $dataFile = 'data.json';
-$jsonData = json_decode(file_get_contents($dataFile), true);
+$data = json_decode(file_get_contents($dataFile), true);
 
-// Handle delete
-if (isset($_GET['delete'])) {
-    $section = $_GET['section'];
-    $index = intval($_GET['index']);
-    array_splice($jsonData[$section], $index, 1);
-    file_put_contents($dataFile, json_encode($jsonData, JSON_PRETTY_PRINT));
-    echo "✅ Item deleted successfully";
-    exit;
-}
-
-// Handle Add
 $section = $_POST['section'];
+$action = $_POST['action'];
 
-if ($section === 'rooms') {
-    // Upload image
-    $targetDir = "uploads/";
-    if (!file_exists($targetDir)) mkdir($targetDir);
-    $imagePath = $targetDir . basename($_FILES["image"]["name"]);
-    move_uploaded_file($_FILES["image"]["tmp_name"], $imagePath);
-
-    $jsonData['rooms'][] = [
-        "name" => $_POST['roomName'],
-        "price" => $_POST['price'],
-        "roomNumber" => $_POST['roomNumber'],
-        "description" => $_POST['description'],
-        "image" => $imagePath
-    ];
-} else {
-    $jsonData[$section][] = [
+if ($action === 'add') {
+    $data[$section][] = [
         "title" => $_POST['title'],
         "description" => $_POST['description'],
         "date" => $_POST['date']
     ];
+} elseif ($action === 'edit') {
+    $index = $_POST['index'];
+    $data[$section][$index] = [
+        "title" => $_POST['title'],
+        "description" => $_POST['description'],
+        "date" => $_POST['date']
+    ];
+} elseif ($action === 'delete') {
+    $index = $_POST['index'];
+    array_splice($data[$section], $index, 1);
 }
 
-file_put_contents($dataFile, json_encode($jsonData, JSON_PRETTY_PRINT));
-header("Location: adminpanel.html");
+file_put_contents($dataFile, json_encode($data, JSON_PRETTY_PRINT));
+echo "success";
 ?>
